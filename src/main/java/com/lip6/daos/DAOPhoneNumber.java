8295990e50f5fd903b0ba987f23f46a46c8b38a5
Sefkan Beyaz <7124107-Kaanar@users.noreply.gitlab.com>
2022-10-04 21:59:51 +0200

    ServicePhoneNumber OK + DAOPhoneNumber ToVerify + PhoneNumber constructor OK

--- src/main/java/com/lip6/daos/DAOPhoneNumber.java
+++ src/main/java/com/lip6/daos/DAOPhoneNumber.java
@@ -16,7 +16,7 @@ public class DAOPhoneNumber {
 		try {
 			
 			//A vérifier qu'il ne faut pas instancier l'entityManager autre part (il est normalement en singleton)
-			Persistence.createEntityManagerFactory("projetJPA");
+			//Persistence.createEntityManagerFactory("projetJPA");
 			EntityManager em = JpaUtil.getEmf().createEntityManager();
 			
 			PhoneNumber phone = new PhoneNumber(id, phoneKind, phoneNumber, contact);
@@ -35,6 +35,69 @@ public class DAOPhoneNumber {
 			e.printStackTrace();
 		}
 		return success;
+	}
+	
+	public boolean UpdatePhoneNumber(int id, String phoneKind, String phoneNumber, Contact contact) {
+		
+		boolean success = false;
+		try {
+			EntityManager em = JpaUtil.getEmf().createEntityManager();
+			
+			PhoneNumber ph = em.find(PhoneNumber.class, id);
+			EntityTransaction tx = em.getTransaction();
+			tx.begin();
+			
+			em.persist(ph);
+			ph.setPhoneKind(phoneKind);
+			ph.setPhoneNumber(phoneNumber);
+			ph.setContact(contact);
+			
+			tx.commit();
+			em.close();
+			success=true;
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		return success;
+	}
+	
+	public boolean RemovePhoneNumber(int id) {
+		
+		boolean success = false;
+		try {
+			EntityManager em = JpaUtil.getEmf().createEntityManager();
+			
+			PhoneNumber ph = em.find(PhoneNumber.class, id);
+			
+			EntityTransaction tx = em.getTransaction();
+			tx.begin();
+			
+			em.remove(ph);
+			tx.commit();
+			success= true;
+			
+			em.close();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		return success;	
+	}
+	
+	public PhoneNumber getPhoneNumber(int id) {
+		try {
+			EntityManager em = JpaUtil.getEmf().createEntityManager();
+			
+			PhoneNumber ph = em.find(PhoneNumber.class, id);
+			
+			return ph;
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			return new PhoneNumber();
+		}
 	}
 	
 }
--- src/main/java/com/lip6/services/ServicePhoneNumber.java
+++ src/main/java/com/lip6/services/ServicePhoneNumber.java
@@ -3,11 +3,48 @@ package com.lip6.services;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.lip6.daos.DAOPhoneNumber;
 import com.lip6.entities.Contact;
+import com.lip6.entities.PhoneNumber;
 
 public class ServicePhoneNumber {
 
-	void createPhoneNumbers(int id, String phoneKind, String phoneNumber, Contact contact) {
+	void createPhoneNumber(int id, String phoneKind, String phoneNumber, Contact contact) {
 		
+		DAOPhoneNumber DaoPN = new DAOPhoneNumber();
+		boolean success = DaoPN.addPhoneNumber(id, phoneKind, phoneNumber, contact);
+		if (success) 
+			System.out.println("Numéro ajout�!");
+		
+		else 
+			System.out.println("Numéro non ajout�!");
+	}
+	
+	public void DeletePhoneNumber(int id) {
+		DAOPhoneNumber DaoPN = new DAOPhoneNumber();
+		boolean success = DaoPN.RemovePhoneNumber(id);
+		if (success) 
+			System.out.println("Numéro supprimé!");
+		
+		else 
+			System.out.println("Numéro non supprimé!");
+	}
+	
+	public void UpdatePhoneNumber(int id,String phoneKind, String phoneNumber, Contact contact) {
+
+		DAOPhoneNumber DaoPN = new DAOPhoneNumber();
+		boolean success = DaoPN.UpdatePhoneNumber(id, phoneKind, phoneNumber, contact);
+		if (success) 
+			System.out.println("Numéro ajout�!");
+		
+		else 
+			System.out.println("Numéro non ajout�!");
+	}
+	
+	public void getPhoneNumber(int id) {
+		DAOPhoneNumber DaoPN = new DAOPhoneNumber();
+		PhoneNumber ph = DaoPN.getPhoneNumber(id);
+		
+	    System.out.println(ph.getId() + ph.getPhoneKind() + ph.getPhoneNumber() + ph.getContact());
 	}
 }
