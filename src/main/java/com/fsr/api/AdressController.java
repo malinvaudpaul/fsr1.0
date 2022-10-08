c47141f556706067408c9358ed2a0e70137d8e83
Sefkan Beyaz <7124107-Kaanar@users.noreply.gitlab.com>
2022-10-08 18:57:21 +0200

    suppression des servlets

--- src/main/java/com/fsr/api/AdressController.java
+++ src/main/java/com/fsr/api/AdressController.java
@@ -5,7 +5,10 @@ import com.fsr.services.ServiceAddress;
 import java.net.URI;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,7 +48,7 @@ public class AddressController {
     return serviceAddress.readAll();
   }
 
-  @UpdateMapping(path = "/{id}", consumes = "application/json", produces = "application/json")
+  @PatchMapping(path = "/{id}", consumes = "application/json", produces = "application/json")
   public Addresss updateAddress(@PathVariable("id") int id, @RequestBody Address address) {
     address.setId(id);
     return serviceAddress.update(address);
